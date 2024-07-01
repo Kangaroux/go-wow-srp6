f3abda8d913e2cfa0e5fdf38713b28a69c76aa3e
Jessie <2302541+Kangaroux@users.noreply.github.com>
2024-07-01 02:07:13 -0400

    Add Encode function for building server headers

--- header/wrath.go
+++ header/wrath.go
@@ -8,6 +8,17 @@ import (
 	"sync"
 )
 
+const (
+	// 23 bits + 1 bit for LARGE_HEADER_FLAG
+	sizeFieldMaxValue = 0x7FFFFF
+
+	// 15 bits (16th bit is reserved for LARGE_HEADER_FLAG)
+	largeHeaderThreshold = 0x7FFF
+
+	// Set on MSB of size field (first header byte)
+	largeHeaderFlag = 0x80
+)
+
 var (
 	wrathDecryptKey = []byte{
 		0xC2, 0xB3, 0x72, 0x3C, 0xC6, 0xAE, 0xD9, 0xB5,
@@ -18,7 +29,8 @@ var (
 		0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57,
 	}
 
-	ErrCryptoNotInitialized = errors.New("header crypto has not been initialized")
+	ErrCryptoNotInitialized = errors.New("srp/header: header crypto has not been initialized")
+	ErrHeaderSizeTooLarge   = errors.New("srp/header: header size is too large")
 )
 
 // WrathHeader is used for encrypting/decrypting world packet headers in WoTLK.
@@ -29,6 +41,45 @@ type WrathHeader struct {
 
 	decryptMutex sync.Mutex
 	encryptMutex sync.Mutex
+}
+
+// Encode returns a header with opcode and size. Encode expects size to not include the
+// 2 bytes for the opcode, and will add +2 to size. In WoTLK, server headers can be either
+// 4 or 5 bytes. If the client is authenticated, [Encrypt] must be used on the encoded header.
+func (h *WrathHeader) Encode(opcode uint16, size uint32) ([]byte, error) {
+	// Include the opcode in the size
+	size += 2
+
+	if size > sizeFieldMaxValue {
+		return nil, ErrHeaderSizeTooLarge
+	}
+
+	var header []byte
+
+	// The size field in the header can be 2 or 3 bytes. If the size field is 3 bytes, the MSB of the
+	// size will be set.
+	//
+	// The header format is: <size><opcode>
+	// <size> is 2-3 bytes big endian
+	// <opcode> is 2 bytes little endian
+	if size > largeHeaderThreshold {
+		header = []byte{
+			byte(size>>16) | largeHeaderFlag,
+			byte(size >> 8),
+			byte(size),
+			byte(opcode),
+			byte(opcode >> 8),
+		}
+	} else {
+		header = []byte{
+			byte(size >> 8),
+			byte(size),
+			byte(opcode),
+			byte(opcode >> 8),
+		}
+	}
+
+	return header, nil
 }
 
 // Init sets up the ciphers using sessionKey. Init must be called before trying to
